e38c864a96f5746df8c0ed80fe819d307951f695
Ricardo Castro <ricardo732@live.com>
2025-08-11 15:10:52 -0300

    update

--- bliblioteca/bliblioteca/settings.py
+++ bliblioteca/bliblioteca/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',  # Assuming 'core' is your main app
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- bliblioteca/core/admin.py
+++ bliblioteca/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Funcionario, Cliente, Livro, Emprestimo
 from django.db.models import Count
 from django.utils.html import format_html
+from import_export.admin import ExportMixin
 
 # Register your models here.
 
@@ -22,20 +23,23 @@ class ClienteAdmin(admin.ModelAdmin):
 class LivroAdmin(admin.ModelAdmin):
     list_display = ['titulo', 'autor', 'editora', 'ano_publicacao', 'quantidade']
     search_fields = ['titulo', 'autor']
-    
+
 @admin.register(Emprestimo)
-class EmprestimoAdmin(admin.ModelAdmin):
+class EmprestimoAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ['cliente', 'livro', 'data_retirada', 'data_devolucao_prevista', 'data_devolucao_real', 'multa']
 
     change_list_template = "admin/emprestimos_changelist.html"
 
-    def changelist_view(self, request, extra_context=None):
-        livros_populares = (
-            self.model.objects
-            .values('livro__titulo')
-            .annotate(total=Count('id'))
-            .order_by('-total')[:5]
-        )
-        extra_context = extra_context or {}
-        extra_context['livros_populares'] = livros_populares
-        return super().changelist_view(request, extra_context=extra_context)
+def changelist_view(self, request, extra_context=None):
+    from django.db.models import Count
+    livros_populares = (
+        self.model.objects
+        .values('livro__titulo')
+        .annotate(total=Count('id'))
+        .order_by('-total')[:5]
+    )
+    extra_context = extra_context or {}
+    extra_context['livros_populares'] = livros_populares
+    return super().changelist_view(request, extra_context=extra_context)
+
+
